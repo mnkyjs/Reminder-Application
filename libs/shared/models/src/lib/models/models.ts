4abd264c91fec0d4fceebe75bb10c36efc8b7729
mnkyjs <71262926+mnkyjs@users.noreply.github.com>
2026-01-17 19:22:09 +0100

    feat: add task model and interfaces

--- libs/shared/models/src/lib/models/models.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-models',
-  imports: [],
-  templateUrl: './models.html',
-  styleUrl: './models.css',
-})
-export class Models {}
